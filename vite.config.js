a684ac278ce0146d06eaba8847c88196cc3ba6dd
OnxCode76 <dariuszkrasicki@gmail.com>
2025-08-26 22:24:18 +0200

    build(vite): add staticCopy for favicons and setup partials/favicons

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-              'resources/scss/app.scss',
-              'resources/scss/auth.scss',
-              'resources/scss/icons.scss',
-              'resources/scss/landing.scss',
-              'resources/js/app.js'
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/scss/app.scss',
+        'resources/scss/auth.scss',
+        'resources/scss/icons.scss',
+        'resources/scss/landing.scss',
+        'resources/js/app.js'
+      ],
+      refresh: true,
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'resources/images',
+          dest: './../',
+        }
+      ]
+    })
+  ],
 });
